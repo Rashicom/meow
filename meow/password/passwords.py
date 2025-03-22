ae012a14d7ec893c2a024f648daab3d07b24ecc4
Rashicom <rashid.kp484@gmail.com>
2025-03-22 20:41:06 +0530

    chore(meow)

--- meow/password/passwords.py
+++ meow/password/passwords.py
@@ -13,3 +13,28 @@ def get_random_password(type="alphanumeric", special_char=False, length=8):
     password = ''.join(random.choice(characters) for _ in range(length))
 
     return password
+
+
+def generate_memmory_password():
+    """
+    This method generate memmorizable password using animals words with random numbers
+    read animal names from animals.txt, replace space with undersquares
+    and then add random numbers to the end.
+
+    """
+    with open("meow/data/animals.txt", "r") as file:
+        animals = file.read().splitlines()
+    with open("meow/data/positiveadjective.txt", "r") as file:
+        positive_adjective = file.read().splitlines()
+
+    # Select a random animal name and replace space with underscores
+    animal_name = random.choice(animals).replace(" ", "_")
+
+    # Select a random positive adjective
+    positive_adjective = random.choice(positive_adjective)
+
+    # Generate a secret
+    secret = get_random_password(length=4)
+
+    # Combine the animal name, positive adjective, and secret and return the result
+    return f"{positive_adjective}_{animal_name}_{secret}"
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,3 +1,16 @@
+from meow.password import get_random_password
+import string
 
 def test_simple():
     assert 1==1
+
+def test_simple_password():
+    simple_pass = get_random_password()
+    assert len(simple_pass) == 8
+    assert len(get_random_password(length=4))==4
+
+
+def test_special_char_password():
+    password = get_random_password(special_char=True)
+    assert len(password) == 8
+    assert any(char in string.punctuation for char in password)
